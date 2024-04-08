33ae3db3dd61578940b795c8fa3a10166e4da980
Syed Aman <syednashikaman@gmail.com>
2024-04-08 07:34:07 -0400

    cin

--- client.cpp
+++ client.cpp
@@ -3,14 +3,8 @@
 #include <string>
 #include <thread>
 #include <array>
-#include <random>
-#include <chrono>
 
 using boost::asio::ip::udp;
-
-std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-std::uniform_int_distribution<int> damage_distribution(1, 10); // Damage range between 1 and 10
-std::uniform_int_distribution<int> sleep_distribution(1, 5); // Sleep between 1 and 5 seconds
 
 void listen_for_updates(udp::socket& socket) {
     std::array<char, 1024> recv_buf;
@@ -43,14 +37,13 @@ int main() {
             }
         });
 
-        // Simulate taking random damage at random intervals
-        while (true) {
-            int damage = damage_distribution(generator);
+        std::cout << "Enter damage taken: ";
+
+        int damage;
+        while (std::cin >> damage) {
             std::string message = "Damage: " + std::to_string(damage);
             socket.send_to(boost::asio::buffer(message), receiver_endpoint);
-
-            int sleep_time = sleep_distribution(generator);
-            std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
+            std::cout << "Enter next damage taken: ";
         }
 
         if(listener_thread.joinable()) {
